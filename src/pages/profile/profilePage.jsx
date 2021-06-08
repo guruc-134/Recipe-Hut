84d205f51e8209f3d04aad10ca8e8abcadeeb03d
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-08 23:32:04 +0530

    feature added added a feature of displaying and removing the favourited items/ dishes

--- src/pages/profile/profilePage.jsx
+++ src/pages/profile/profilePage.jsx
@@ -1,18 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useContext} from 'react'
 import './profilePage.style.scss'
+import {firestore} from '../../backend/firebase/firebase.utils';
+import { UserContext } from '../../context/userContext';
+import Card from '../../components/displayCard/card.component';
 const ProfilePage = () => {
-    const [userName, setUserName ] = useState("Guru Charan")
-    const [streak,setStreak] = useState(10)
+    const user = useContext(UserContext)
+    const [favs,setFavs] = useState([])
+    const getuserDataFromFireStore = () =>
+    {
+        var store = new Set()
+        firestore.collection(`/users/${user.id}/favourites`).get()
+        .then((snapshot) =>
+        {
+            snapshot.docs.map( item =>
+                {
+                    store.add({["itemStoreid"]:item.id, ...item.data()})
+                    
+                })
+                var storeArr = [...store]
+                storeArr.sort((a,b)=>(
+                    Date.parse(a.pushedAt) > Date.parse(b.pushedAt)
+                ))
+                setFavs(storeArr)
+        })
+    }
     return (
         <div className='profile'>
             <h1> Profile Page</h1>
                 <div className='profile-person'>
-                    <h3>Name:{userName}</h3>
-                    <p> site visit streak : {streak} </p>
+                    <h3>Name:{user?user.displayName:'login' }</h3>
+                    <p> site visit streak : 10 </p>
+                    <h3>Email: {user? user.email : 'login' }</h3>
+                    <p> Joined on Date: 
+                                            </p>
                 </div>
-                <div className ='profile-favourites'>
+                <div className ='profile-favourites' onClick={getuserDataFromFireStore}>
                     <h3>favourites</h3>
                     <p> This section contains the names of dishes liked, favourited by the user</p>
+                    <div className='fav-items'>
+                        {
+                           favs.map((item) => {
+                            return(
+                                item.found?
+                        <Card key = {item.id} recipe = {item} fromFavs = {true} />:null)
+                        })
+                        }
+                    </div>
                 </div>
                 <p>
                 you can also write notes and store them here

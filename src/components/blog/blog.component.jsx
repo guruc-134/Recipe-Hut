795fceac504134ec8c3212f38ec27ad3f5aabd68
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-16 00:37:38 +0530

    configured firestore with blogs page, all blogs in the same date will be stored in one page

---
+++ src/components/blog/blog.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blogs() {
+    return (
+        <div>
+            This is a blog
+        </div>
+    )
+}
+
+export default Blogs
--- src/pages/community/communityPage.jsx
+++ src/pages/community/communityPage.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-
+import React , {useState, useContext} from 'react'
+import firebase , {firestore} from '../../backend/firebase/firebase.utils';
+import { UserContext } from '../../context/userContext';
 function CommunityPage() {
+    // const user = useContext(UserContext)
+    const [postHeader, setPostHeader] = useState("")
+    const [blogContent, setBlogContent] = useState("")
+
+    const submitPost = (e) =>
+    {
+        e.preventDefault();
+        var blogObject = {
+            header:postHeader,
+            content:blogContent
+        }
+        var today = new Date()
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today = dd + '-' + mm + '-' + yyyy;
+        console.log(today)
+        firestore.collection('blogs').doc(today).collection('blogs').add(blogObject)
+        setBlogContent("")
+        setPostHeader("")
+    }
+    const handleChange =(e)=>
+    {
+        const {name,value}  = e.target
+        if (name === 'heading') setPostHeader(value)
+        if (name === 'blogContent') setBlogContent(value)
+
+
+    }
     return (
         <div>
             <h1>
                 This is the community page
                 </h1>
+                create and post new recipes
+                <form class='blog-form'>
+                    
+                    <div>
+                    <input onChange= {handleChange} 
+                    name = 'heading' className ='form-inputs' 
+                    type = 'text' placeholder='heading' value ={postHeader}></input>
+                    </div>
+                    
+                    <div> 
+                    <textarea onChange= {handleChange} 
+                    name ='blogContent' className='form-inputs' 
+                    rows='5' cols='25' placeholder =' write your own recipe'
+                    value={blogContent}>
+                    </textarea>
+
+                    </div>
+                    <p> upload picture</p>
+                    <input type ='file'></input>
+                    <button onClick={submitPost}> Post !</button>
+                </form>
         </div>
     )
 }

0144d3b66da512a97704b2b9ecc992a1ed6d3f8d
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-18 12:54:29 +0530

    data storage, feature addition -> pushed all trivia to  firestore -> installed react paginate -> configured pagination -> added pagination to the homePage

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -2,22 +2,35 @@ import React, { useState,useEffect } from 'react'
 import Bot from '../../components/Finders/bot/bot.component';
 import Card from '../../components/displayCard/card.component'
 import './homePage.style.scss'
+import '../../sass/pagination.styles.scss'
 import Chef  from '../../components/Finders/chef/chef.component.jsx'
 import  {firestore} from '../../backend/firebase/firebase.utils';
+import ReactPaginate from 'react-paginate';
 // import axios from 'axios'
 // import '../../backend/spoonacular/recipes'
 const HomePage = () => {
     const [homeRecipes,setHomeRecipes] = useState([])
+    const [pageNumber, setPageNumber] = useState(0)
+    const   itemsPerPage = 20
+    const pagesVisited = pageNumber * itemsPerPage
+
+    // function  for pagination
+    const displayItems = homeRecipes.slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map( item =>
+        (
+        item.found?
+        <Card key = {item.id} recipe = {item}/>:null))
+    
     function shuffle(array) {
         return array.sort(() => Math.random() - 0.5);
-      }
+    }
     const getRecipesFromFireStore = (index) =>
     {
         var array = []
         firestore.collection('home-PageRecipes').get()
             .then( snapShot =>
             {
-               var  doc = snapShot.docs[index]
+            var  doc = snapShot.docs[index]
                     var dd = doc.data()
                     array.push(dd.recipeResults)
                     console.log(array.length)
@@ -30,6 +43,11 @@ const HomePage = () => {
         getRecipesFromFireStore(3);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+    var pageCount = Math.ceil(homeRecipes.length / itemsPerPage)
+    const changePage = ({selected}) =>
+    {
+        setPageNumber(selected)
+    }
     return (
         <div className='home-page'>
             <h1> Home Page</h1>
@@ -37,15 +55,22 @@ const HomePage = () => {
             <div className='recipe-cards'>
             {
                 homeRecipes?
-                homeRecipes.map((item) => {
-                    return(
-                        item.found?
-                <Card key = {item.id} recipe = {item}/>:null)
-                }) : null
+                displayItems: null
             } 
             </div>
-            <Bot/> 
+            <ReactPaginate
+                previousLabel = {"previous"}
+                nextLabel = {"next"}
+                pageCount = {pageCount}
+                onPageChange = {changePage}
+                containerClassName = {"paginationBtns"}
+                previousLinkClassName = {"previousBtn"}
+                nextLinkClassName = {"nextBtn"}
+                disabledClassName = {"paginationDisabled"}
+                activeClassName  = {"paginationActive"}
 
+            />
+            <Bot/> 
         </div>
     )
 }

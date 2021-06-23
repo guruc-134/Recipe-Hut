e210bb13a4f5013f6006339fe99f81053a656fe1
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-23 19:35:38 +0530

    modified signin signup component conditionally displaying signin and signup

--- src/pages/signin-signout/signin-signup.component.jsx
+++ src/pages/signin-signout/signin-signup.component.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Signin from '../../components/signin/signin.component'
 import Signup from '../../components/signup/signup.component'
 import './signin-signup.style.scss'
+import {Link} from 'react-router-dom'
+
 function SigninSignout() {
+    const showSignin = () =>{
+        var signinDiv = document.querySelector(".signin-div")
+        if(signinDiv) signinDiv.style.display="inline-block"
+        var signupDiv = document.querySelector(".signup-div")
+        if(signupDiv) signupDiv.style.display="none"
+    }
+    const showSignup = () =>{
+        var signupDiv = document.querySelector(".signup-div")
+        if(signupDiv) signupDiv.style.display="inline-block"
+        var signinDiv = document.querySelector(".signin-div")
+        if(signinDiv) signinDiv.style.display="none"
+    }
+    
     return (
         <div className='signin-signup'>
-            <Signin/>
-            <Signup/>
+            <nav className='signin-signup-nav'>
+                <div onClick ={showSignin}>
+                Signin
+                </div>
+                <div onClick = {showSignup}>
+                Sign up
+                </div>  
+            </nav>
+            <div className='signin-div'>
+                <Signin/>
+            </div>
+            <div className='signup-div'>
+                <Signup/>
+            </div>
+                
         </div>
     )
 }

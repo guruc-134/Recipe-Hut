a5775263da154a3e58a2ac60afed46bc3ad43091
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-23 19:36:21 +0530

    styled community page

--- src/pages/community/communityPage.jsx
+++ src/pages/community/communityPage.jsx
@@ -1,14 +1,12 @@
 import React , {useState, useContext, useEffect} from 'react'
-import firebase , {firestore} from '../../backend/firebase/firebase.utils';
+import  {firestore} from '../../backend/firebase/firebase.utils';
+import {Link} from 'react-router-dom'
 import { UserContext } from '../../context/userContext';
-import ReactDOM from "react-dom";
+
+import './communityPage.styles.scss'
 function CommunityPage() {
     const user = useContext(UserContext)
     const [pageBlogs, setPageBlogs] = useState("")
-    const [postHeader, setPostHeader] = useState("")
-    const [blogContent, setBlogContent] = useState("")
-    const [blogFile, setBlogFile] = useState("")
-
     const getBlogsFromFireBase = () =>
     {
             var today = new Date()
@@ -28,103 +26,15 @@ function CommunityPage() {
                         setPageBlogs(array)
                 })
     }
-
-    const resetStates  = () =>
-    {
-        //  preventing rerendering after each state change
-        ReactDOM.unstable_batchedUpdates( () =>
-        {
-            setBlogContent("")
-            setPostHeader("")
-            setBlogFile("")
-        })
-    }
-    const submitPost = (e) =>
-    {
-        // console.log(blogFile[0])
-        if (blogContent === "" || blogContent === undefined || blogFile === "")
-        {
-            console.log('not a valid input')
-        }
-        e.preventDefault();
-        var blogObject = {
-            header:postHeader,
-            content:blogContent,
-            files:blogFile,
-            postedBy:user
-        }
-        var today = new Date()
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
-        today = dd + '-' + mm + '-' + yyyy;
-        console.log(today)
-        firestore.collection('blogs').doc(today).collection('daily_blogs').add(blogObject)
-        resetStates()
-        getBlogsFromFireBase()
-
-    }
-    const convertToBase64 = (image) => {
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(image);
-    
-        return new Promise((resolve, reject) => {
-            fileReader.onload = () => {
-                resolve(fileReader.result);
-            }
-    
-            fileReader.onerror = () => {
-                reject(fileReader.error);
-            }
-        })
-    }
-    
-    const handleChange = async (e)=>
-    {
-        const {name,value,files}  = e.target
-
-        if (name === 'heading') setPostHeader(value)
-        if (name === 'blogContent') setBlogContent(value)
-        else if (name === 'blogFile') 
-        {
-            var Img_file = await convertToBase64(files[0])
-            setBlogFile(Img_file)
-        }   
-    }
     useEffect(() => {
         getBlogsFromFireBase()
     }, [])
 
     return (
-        <div>
-            <h1 className ="heading-primary">
-                This is the community page
-                </h1>
-                create and post new recipes
-                <form className='blog-form'>
-                    
-                    <div>
-                    <input onChange= {handleChange} 
-                    name = 'heading' className ='form-inputs' 
-                    type = 'text' placeholder='heading' value ={postHeader}
-                    required
-                    />
-                    </div>
-                    <div> 
-                    <textarea onChange= {handleChange} 
-                    name ='blogContent' className='form-inputs' 
-                    rows='5' cols='25' placeholder =' write your own recipe'
-                    value={blogContent}
-                    required
-                    />
-                    </div>
-                    <p> upload picture</p>
-                    <input name='blogFile' type ='file' accept='image/gif, image/jpeg, image/png' onChange ={handleChange} multiple></input>
-                    {blogFile?<img src = {blogFile} alt ='recipe file' width="100"/>
-                    :null}
-                    <button type ='submit' onClick={submitPost}> Post !</button>
-                </form>
-
+        <div className = "community-page">
+            <Link to='/community/write'>
+                <h3> Share your recipes</h3>
+            </Link>
                 {/*  displaying already written recipe blogs */}
                 <div className = 'blog-displayer'>
                     {
@@ -133,7 +43,7 @@ function CommunityPage() {
                                 {console.log(blog)} 
                                 <h2>{blog.header}</h2>
                                { blog.files?<img src = {blog.files} alt = 'blogPIc'/>:null}
-                                <p>{blog.content}</p>
+                                <p class='blog-displayer-content'>{blog.content}</p>
                                 <b><p>posted by : {blog.postedBy.displayName}</p></b>
                             </div>
                             )): null

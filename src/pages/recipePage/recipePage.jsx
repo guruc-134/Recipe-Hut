8d6836133837d5af989aaa9cafacc35f82600669
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-26 01:47:09 +0530

    added dual search-nature in the recipe page -> search from indexed firebase collections -> search recipes from API using recipeId

--- src/pages/recipePage/recipePage.jsx
+++ src/pages/recipePage/recipePage.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React ,{useEffect, useState} from 'react'
+import  {firestore} from '../../backend/firebase/firebase.utils';
+import './recipePage.styles.scss';
+import axios from 'axios'
+const APIKEY = ['8ab5fa53ef8f45d3a3d5c00e6966c9a3',
+'cc548ac21e2642999689cdff7acb3468',
+'8e743479e9f9467795afffbb26844379',
+'c7a232f614b84f2bb1df4d3d7a3bc567',
+'4a5b12ec6f3d4159b4b160e8808f4601',
+'9a0f78a9a9fb4e1ba5857d871f42f1a8']
+const baseUrl = 'https://api.spoonacular.com';
+function RecipePage(props) {
+    const [recipe,setRecipe] = useState("")
+    const extractDetails = () =>{
+        console.log(props.match.params.recipeId)   
+        const [docId,recipeId,index] = props.match.params.recipeId.split('-')
+        return {'docId':docId,'recipeId':recipeId,'index':index}
+    }
+    const getRecipe = () =>{
+        var {docId,recipeId,index} = extractDetails()
+        var recipeObject = {}
+        if(docId==='search'){
+            // console.log(`https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions?stepBreakdown=true&apiKey=${APIKEY[1]}`)
+            axios.get(`https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions?stepBreakdown=true&apiKey=${APIKEY[1]}`)
+            .then(item=>{
+                var grpobj =[]
+               item.data[0].steps.map(result =>{
+                    // console.log(result)
+                    grpobj.push({ingredients:result.ingredients,step:result.step})
+                })
+                var image =`https://spoonacular.com/recipeImages/${recipeId}-312x231.jpg`;
+                recipeObject= {grpObj:grpobj,image:image}
+            })
+            .catch(err=>console.log(err))
+            //  summary 
+            // console.log(`https://api.spoonacular.com/recipes/${recipeId}/summary?apiKey=${APIKEY[1]}`)
+            axios.get(`https://api.spoonacular.com/recipes/${recipeId}/summary?apiKey=${APIKEY[1]}`)
+            .then(resp =>{
+                resp = resp.data
+                recipeObject = {id:resp.id,title:resp.title,summary:resp.summary,...recipeObject}
+                setRecipe(recipeObject)
+            })
+            // 
+        }
+        else{
 
-function recipePage() {
+            firestore.collection('home-PageRecipes').doc(docId).get()
+            .then( response =>{
+                console.log(response.data().recipeResults[index])
+                setRecipe(response.data().recipeResults[index])
+            })
+        }
+
+    }
+    useEffect(()=>
+    {
+        var summary = document.querySelector('.summary')
+        if(summary) summary.innerHTML = recipe.summary
+    },[recipe])
+    useEffect(()=>{
+        getRecipe()
+    },[])
     return (
-        <div>
-            <h1> Recipe Page</h1>
+        <div className='recipePage'>
+            {console.log(props.location,props.location.pathname)}
+            {/* <Link to=""></Link> */}
+            {
+            recipe?
+            <div>
+                {console.log(recipe)}
+                <div className='headerDiv'>
+                <h1>{recipe.title}</h1>
+                <img src = {recipe.image} alt='recipeImg'></img>
+                </div>
+
+                <div>
+                    <h3>Recipe Summary</h3>
+                    <p className='summary'>{recipe.summary}</p>
+                </div>
+                <h3> Recipe</h3>
+                <div className='recipe'>
+                    {
+                    recipe.grpObj.map(item=>{
+                        return (
+                        <div className='steps'>
+                            <h4>
+                                <i className="fas fa-utensils"></i>
+                                Ingredients and Steps
+                            </h4>
+                                {
+                                    item.ingredients.map( ingr =>(
+                                        <span>{ingr.name}</span>
+                                    ) )
+                                }
+                            <p>{item.step}</p>
+                            <hr></hr>
+                        </div>
+                        ) 
+                    })
+                    
+                    
+                    }
+                </div>
+            </div>
+            :<h1> The recipe is not available</h1>
+            }
+
         </div>
     )
 }
 
-export default recipePage
+export default RecipePage

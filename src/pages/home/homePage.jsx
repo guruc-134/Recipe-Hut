c4d2326201752d086bdbad8b6442d52281706445
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-26 01:45:43 +0530

    added feature to route to recipePage through home page recipes

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -3,6 +3,7 @@ import Bot from '../../components/Finders/bot/bot.component';
 import Card from '../../components/displayCard/card.component'
 import './homePage.style.scss'
 import '../../sass/pagination.styles.scss'
+import {Link} from 'react-router-dom'
 import Chef  from '../../components/Finders/chef/chef.component.jsx'
 import  {firestore} from '../../backend/firebase/firebase.utils';
 import ReactPaginate from 'react-paginate';
@@ -11,36 +12,50 @@ import ReactPaginate from 'react-paginate';
 const HomePage = () => {
     const [homeRecipes,setHomeRecipes] = useState([])
     const [pageNumber, setPageNumber] = useState(0)
-    const   itemsPerPage = 12
+    const [docId, setDocId] = useState("")
+    const [index,setIndex] = useState(3)
+    const   itemsPerPage = 12;
     const pagesVisited = pageNumber * itemsPerPage
 
     // function  for pagination
     const displayItems = homeRecipes.slice(pagesVisited, pagesVisited + itemsPerPage)
     .map( item =>
-        (
-        item.found?
-        <Card key = {item.id} recipe = {item}/>:null))
+        {
+            return(
+                item.ele?
+                <Card key = {item.ele.id} recipe = {item.ele} docId ={docId} index={item.ind}/>:null
+                );
+        })  
     
     function shuffle(array) {
         return array.sort(() => Math.random() - 0.5);
+    }
+    const markArray =(arr) =>{
+        var newArr = []
+        arr.map((ele,index)=>{
+            newArr.push({['ind']:index,['ele']:ele})
+        })
+        return newArr
     }
     const getRecipesFromFireStore = (index) =>
     {
         var array = []
         firestore.collection('home-PageRecipes').get()
             .then( snapShot =>
-            {
-            var  doc = snapShot.docs[index]
+                {
+                    var  doc = snapShot.docs[index]
+                    setDocId(doc.id)
                     var dd = doc.data()
-                    array.push(dd.recipeResults)
-                    // console.log(array.length)
-                    setHomeRecipes(shuffle(array[array.length-1]))
+                    array= dd.recipeResults
+                    array = markArray(array)
+                    console.log(array)
+                    setHomeRecipes(shuffle(array))
                 
             })
             .catch(error => console.log('error fetching the data from firebase',error))
     }
     useEffect(() => {
-        // getRecipesFromFireStore(3);
+        getRecipesFromFireStore(index);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     var pageCount = Math.ceil(homeRecipes.length / itemsPerPage)

02fcd83bc98a11d803e5f5e618915a15d49258a0
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-26 01:44:50 +0530

    added calender filter feature

--- src/pages/community/communityPage.jsx
+++ src/pages/community/communityPage.jsx
@@ -1,21 +1,30 @@
-import React , {useState, useContext, useEffect} from 'react'
+import React , {useState, useEffect} from 'react'
 import  {firestore} from '../../backend/firebase/firebase.utils';
 import {Link} from 'react-router-dom'
-import { UserContext } from '../../context/userContext';
-
+import BlogCard from '../../components/displayCard/blog/blog.component';
 import './communityPage.styles.scss'
 function CommunityPage() {
-    const user = useContext(UserContext)
+    // const user = useContext(UserContext)
     const [pageBlogs, setPageBlogs] = useState("")
-    const getBlogsFromFireBase = () =>
+    const obtainDate = (date) =>{
+        var dd = String(date.getDate()).padStart(2, '0');
+        var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = date.getFullYear();
+        date = yyyy +'-'+mm+'-'+dd;
+        return date
+    }
+    const [date, setDate] = useState(obtainDate(new Date()))
+    const getBlogsFromFireBase = (date) =>
     {
-            var today = new Date()
-            var dd = String(today.getDate()).padStart(2, '0');
-            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-            var yyyy = today.getFullYear();
-            today = dd + '-' + mm + '-' + yyyy;
+        console.log('passed date',date)
             const array = []
-            firestore.collection('blogs').doc(today).collection('daily_blogs').get()
+            console.log('fetching from firebase')
+            var today = obtainDate(new Date())
+            var searchDate = today 
+            if(date != null)
+            searchDate = date
+            console.log('searchDate',searchDate)
+            firestore.collection('blogs').doc('daily_blogs').collection(searchDate).get()
             .then( blogList =>
                 {
                     blogList.docs.map( item =>
@@ -29,23 +38,35 @@ function CommunityPage() {
     useEffect(() => {
         getBlogsFromFireBase()
     }, [])
+    const handleChange = (e) =>{
+        console.log('date changed',e.target.value)
+        setDate(e.target.value)
+    }
+    const submitForm = (e) =>{
+        e.preventDefault();
+            getBlogsFromFireBase(date)
+    }
 
     return (
         <div className = "community-page">
-            <Link to='/community/write'>
-                <h3> Share your recipes</h3>
-            </Link>
+            <div className = 'community-page-write-div'>
+                <Link className = 'community-page-write' to='/community/write'>
+                    <h3> Share your recipes <i className="ri-ball-pen-fill"></i></h3>
+                </Link>
+            </div>
+            <div className='community-page-sidebar'>
+                get recipes by date
+                <form className='date-form'onSubmit = {submitForm}>
+                <input type='date' value={date} onChange={handleChange} min="2021-06-24" max ={obtainDate( new Date())}/>
+                <button type='submit'>fetch</button>
+                </form>
+
+            </div>
                 {/*  displaying already written recipe blogs */}
                 <div className = 'blog-displayer'>
                     {
                         pageBlogs?(pageBlogs.map( blog=> 
-                            <div key = {blog.id}>
-                                {console.log(blog)} 
-                                <h2>{blog.header}</h2>
-                               { blog.files?<img src = {blog.files} alt = 'blogPIc'/>:null}
-                                <p class='blog-displayer-content'>{blog.content}</p>
-                                <b><p>posted by : {blog.postedBy.displayName}</p></b>
-                            </div>
+                            <BlogCard blog={blog} key={blog.id}/>
                             )): null
                     }
                 </div>

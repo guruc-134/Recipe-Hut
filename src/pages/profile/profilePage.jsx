efb9e29d753d932bba5b91002c2a6979136caf5d
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-30 01:27:44 +0530

    created profile page and integrated with userId links, enabled profile sharing and profile viewing modes

--- src/pages/profile/profilePage.jsx
+++ src/pages/profile/profilePage.jsx
@@ -1,14 +1,31 @@
-import React, { useState, useContext} from 'react'
+import React, { useState, useContext, useEffect} from 'react'
+import { useHistory,useLocation, useParams } from "react-router-dom";
 import './profilePage.style.scss'
 import {firestore} from '../../backend/firebase/firebase.utils';
 import { UserContext } from '../../context/userContext';
 import Card from '../../components/displayCard/card.component';
 import {auth} from '../../backend/firebase/firebase.utils'
 
-const ProfilePage = () => {
+const ProfilePage = (params) => {
     const user = useContext(UserContext)
+    const handle  = useParams()
+    const location = useLocation()
+    const [fromNav,setFromNav] = useState(true)
+    const [incomingUser,setIncomingUser] = useState() 
     const [favs,setFavs] = useState([])
     const [history,setHistory] = useState([])
+    const extractDetails = () =>{
+        var queryString = handle.userId.split("-")
+        firestore.doc(`/users/${queryString[1]}`).get()
+        .then(resp=>{
+            setIncomingUser(resp.data())
+        })
+    }
+
+    const getUserInfo = () =>{
+        setFromNav(location.state.fromNavbar)
+        extractDetails()
+    }
     const getSearchHistory = () =>
     {
         const arr = []
@@ -21,9 +38,30 @@ const ProfilePage = () => {
                     arr.push({id:item.id, ...item.data()})
                     
                 })
-                
                 setHistory(arr)
         })
+    }
+    const hideFavItems = () =>
+    {
+        var favItems = document.querySelector(".fav-items")
+        if(favItems) favItems.style.display ="none"
+
+    }
+    const showFavItems = () =>
+    {
+        var favItems = document.querySelector(".fav-items")
+        if(favItems) favItems.style.display ="flex"
+    }
+    const hideHistoryItems = () =>
+    {
+        var HistoryItems = document.querySelector(".history-items")
+        if(HistoryItems) HistoryItems.style.display ="none"
+
+    }
+    const showHistoryItems = () =>
+    {
+        var HistoryItems = document.querySelector(".history-items")
+        if(HistoryItems) HistoryItems.style.display ="flex"
     }
     const getuserDataFromFireStore = () =>
     {
@@ -44,18 +82,55 @@ const ProfilePage = () => {
                 setFavs(storeArr)
         })
     }
+    const signOut =()=>{
+        // eslint-disable-next-line no-restricted-globals
+        if(confirm("Are you sure, you want to logout"))
+        auth.signOut()
+    }
+    useEffect(()=>{
+        // getuserDataFromFireStore()
+        // getSearchHistory()
+        getUserInfo()
+    },[fromNav,location])
     return (
         <div className='profile'>
-            <h1 className='heading-primary'> Profile</h1>
+            <h1 className='heading-primary heading'> Profile</h1>
+            {
+                
+                !fromNav?
+                
+                <div className = 'profile-details'>
+                    {
+                        incomingUser?<div>
+                        <span className={`${incomingUser.isAdmin?"admin-tag":"user-tag"}`}>{incomingUser.isAdmin?"Admin":"User"}</span>
+                        <h3><span>Name:</span>{incomingUser.displayName}</h3>
+                        <h3><span>Display Name:</span> Cherry</h3>
+                        <h3><span>Email:</span> {incomingUser.email}</h3>
+                        </div>:null
+                        }
+                </div>:null
+                
+            }
+            {fromNav?
+            (<div>
             <div className = 'profile-details'>
-                <div className='profile-person'>
-                    <h3>Name:{user?user.displayName:'login' }</h3>
-                    <h3>Email: {user? user.email : 'login' }</h3>
+            <span className={`${user.isAdmin?"admin-tag":"user-tag"}`}>{user.isAdmin?"Admin":"User"}</span>
+                    <h3><span>Name:</span>{user?user.displayName:'login' }</h3>
+                    <h3><span>Display Name:</span> Cherry</h3>
+                    <h3><span>Email:</span> {user? user.email : 'login' }</h3>
+            </div>
+                <div>
+                <span className='signOut' onClick= {signOut}> <i class="fas fa-sign-out-alt"></i> Sign out</span>
                 </div>
                 <div className ='profile-favourites' onClick={getuserDataFromFireStore}>
-                    <h3>favourites</h3>
-                    <p> This section contains the names of dishes liked, favourited by the user</p>
+                    <h3 className='favs-heading'>
+                        <i  onClick ={showFavItems} className="fas fa-heart"></i>
+                        favourites                  
+                        <i onClick ={showFavItems}  className="fas fa-heart"></i>
+                        </h3>
+                    {favs.length>0? 
                     <div className='fav-items'>
+                        <i  onClick ={hideFavItems}className="hide fas fa-window-close"></i>
                         {
                             favs.map((item) => {
                                 return(
@@ -63,26 +138,40 @@ const ProfilePage = () => {
                                     <Card className='fav-card' key = {item.id} recipe = {item} fromFavs = {true} />:null)
                                 })
                             }
-                    </div>
+                    </div>:<p>List of all your favourite items</p>}
                 </div>
+
+
                 <div className = 'searchHistory' onClick = {getSearchHistory}>
-                <h3>History</h3>
-                    <div className='history-items'>
+                    <h3>
+                    <i onClick={showHistoryItems}className="fas fa-history"></i>History
+                    <i onClick={showHistoryItems}className="fas fa-history"></i>
+                    </h3>
+                    {history.length>0? <div className='history-items'>
+                        <i  onClick ={hideHistoryItems}className="hide fas fa-window-close"></i>
                         {
                             history.map((item) =>
                             <div key ={item.id}>
-                                {/* {console.log(item)} */}
-                                <p>{item.query}</p>
-                                <p>searched on {item.searchedOn}</p>
+                                <p>
+                                <span className='icon'>
+                                <i className={item.icon}/>
+                                </span>
+                                <span className='query'>
+                                    {item.query} 
+                                </span>
+                                <span className='searchedOn'>
+                                - {item.searchedOn}
+                                </span>
+                                </p>
                             </div>)
                             }
-                    </div>
+                    </div>:<p>your search history</p>}
                 </div>
-                <span className='navbar-links-link' onClick= {()=> auth.signOut()}> Sign out</span>
+                </div>):null
+            }
                 
                 {/*  optional persons bolgs will be present here with the #id tags 
                 so that when we press here in teh a tags wil lead them to that page */}
-        </div>
     </div>
     )
 }

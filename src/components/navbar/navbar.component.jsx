1f006bd28bdbe10224c44636b957ab5c31a82262
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-06-30 01:31:53 +0530

    feature ( added active classes  and styles )

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import {auth} from '../../backend/firebase/firebase.utils'
 import './navbar.style.scss';
 const navbar = ({currentUser}) => {
+    console.log(currentUser)
+    const selected ={
+        fontWeight:'bold',
+        borderBottom:'2px solid black',
+        color:'green'
+    }
     return (
         <div className='navbar'>
             {/* <div className='header'>
-                <Link className='navbar-links-link' to = '/home'><img className="navbar-header-logo" src={`${process.env.PUBLIC_URL}/logo_RH.png`} alt="page_logo"></img></Link>                
+                <NavLink className='navbar-links-link' to = '/home'><img className="navbar-header-logo" src={`${process.env.PUBLIC_URL}/logo_RH.png`} alt="page_logo"></img></Link>                
             </div> */}
             <div className='navbar-links'>
-                <Link className='navbar-links-link' to = '/home'><i className="ri-home-3-line navbar-links-link"></i></Link>
-                <Link className='navbar-links-link' to = '/search'><i className="ri-search-line navbar-links-link"></i></Link>
+                <NavLink className='navbar-links' activeStyle={selected}  to = '/home'><i className="ri-home-3-line navbar-links-link"></i></NavLink>
+                <NavLink className='navbar-links' activeStyle={selected}  to = '/search'><i className="ri-search-line navbar-links-link"></i></NavLink>
                 {
-                currentUser? <Link className='navbar-links-link' to = '/profile'><i className="ri-user-3-line navbar-links-link"></i></Link>:null
+                currentUser? <NavLink  activeStyle={selected} className='navbar-links' 
+                to={
+                    {
+                        pathname:`/profile/user-${currentUser.id}`,
+                        state:{
+                            fromNavbar:true
+                        }
+                    }
+                }>
+                    <i className="ri-user-3-line navbar-links-link"></i></NavLink>:null
                 }
                 {
-                currentUser? <Link className='navbar-links-link' to = '/community'><i className="ri-discuss-fill navbar-links-link"></i></Link>:null
+                currentUser? <NavLink activeStyle={selected}  className='navbar-links' to = '/community'><i className="ri-discuss-fill navbar-links-link"></i></NavLink>:null
                 }
                 {
                     currentUser ? (null):
-                (<Link className='navbar-links-link' to = '/signin'> 
+                (<NavLink  activeStyle={selected} className='navbar-links' to = '/signin'> 
                 <i className="ri-login-box-line"></i>
-                </Link>)
+                </NavLink>)
                 }
 
             </div>

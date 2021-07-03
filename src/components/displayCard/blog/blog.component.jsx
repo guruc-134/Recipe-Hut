b22bb762937decede8b5b2eb34675e74c23309f9
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-07-04 01:52:13 +0530

    new features are added -> blog liking feature addedd -> comments feature added -> display and redirect to the liked user

--- src/components/displayCard/blog/blog.component.jsx
+++ src/components/displayCard/blog/blog.component.jsx
@@ -3,15 +3,14 @@ import './blog.style.scss'
 import {Link} from 'react-router-dom'
 import { UserContext } from '../../../context/userContext';
 import  {firestore} from '../../../backend/firebase/firebase.utils';
+import ReactDOM from "react-dom";
 function BlogCard({blog}) {
-
     const [readMsg, setReadMsg] = useState("read more")
     const user = useContext(UserContext)
     const [blogComments,setBlogComments] = useState(null)
     const [comment,setComment] = useState("")
     const [likes,setLikes] = useState(new Set())
     const [didLike,setDidLike] = useState(false)
-    // date parser
 
     const readMore = () =>{
         if(readMsg === "read less")
@@ -28,21 +27,27 @@ function BlogCard({blog}) {
     const handleCommentChange = (e) =>{
         setComment(e.target.value)
     }
+    const closeLikesDisplay = () =>{
+        var likesBox = document.querySelector(`#likes_by_displayer-${blog.fbId}`)
+        if(likesBox)
+        likesBox.style.display = 'none'
+        
+    }
+    const openLikesDisplay = () =>{
+        var likesBox = document.querySelector(`#likes_by_displayer-${blog.fbId}`)
+        if(likesBox)
+        likesBox.style.display = 'inline-block'
+        
+    }
     const openCommentBox =()=>{
-        // var commentBoxClose = document.querySelector('.blog-reactions-close')
-        // if(commentBoxClose)
-        // commentBoxClose.style.display='inline-block'
-        var commentBox = document.querySelector(`.blog-comment-input-${blog.id}`)
+        var commentBox = document.querySelector(`#blog-comment-input-${blog.fbId}`)
         if (commentBox)
             commentBox.style.display='inline-block'
-            var commentBoxcc = document.querySelector(`.blog-comment-input`)
-            if (commentBoxcc)
-                commentBoxcc.style.display='inline-block'
         getCommentsFromFb()
         
     }
     const closeCommentBox =()=>{
-        var commentBox = document.querySelector(`.blog-comment-input-${blog.id}`)
+        var commentBox = document.querySelector(`#blog-comment-input-${blog.fbId}`)
         if (commentBox)
             commentBox.style.display='none'
     } 
@@ -58,13 +63,17 @@ function BlogCard({blog}) {
 
         })
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const getLikes = () =>{
         firestore.collection(`blogs/daily_blogs/${blog.date}/${blog.fbId}/likes`).get()
         .then(resp=>{
             var ss = new Set()
             resp.docs.map(item=> ss.add(item.data().id))
-            setDidLike(ss.has(user.id))
-            setLikes(ss)
+            ReactDOM.unstable_batchedUpdates(() => {
+                setDidLike(ss.has(user.id))
+                setLikes(ss)
+
+             })
 
         })
     }
@@ -73,18 +82,19 @@ function BlogCard({blog}) {
         var ss = new Set()
         firestore.collection(`blogs/daily_blogs/${blog.date}/${blog.fbId}/likes`).get()
         .then(resp=>{
-            resp.docs.map(item=> {
+            resp.docs.map(item => {
                 ss.add(item.data().id)
             })
             if(!ss.has(user.id))
                 firestore.collection(`blogs/daily_blogs/${blog.date}/${blog.fbId}/likes`).add({id:user.id})
+            
         })
     }
     useEffect(()=>{
         getLikes()
-    },[])
+    },[likes])
     return (
-        <div className = "blog-card" key = {blog.id}>
+        <div className = "blog-card" key = {blog.fbId}>
             <div className = 'blog-content'>
                 <p className='blog-title'>{blog.header}</p>
                 <div className='blog-content-l'>
@@ -127,7 +137,7 @@ function BlogCard({blog}) {
                             {readMsg === "read less"?
                             (
                                 <span className = 'content-more'
-                                id={`content-more-${blog.id}`}> 
+                                id={`content-more-${blog.fbId}`}> 
                                 {blog.content.substring(100,blog.content.length)}
                                 </span>):null
                             }
@@ -141,63 +151,90 @@ function BlogCard({blog}) {
                     }
                 </div>
                 </div>
+                {/*  */}
                 <div className='blog-reactions'>
                     <div>
-                        <i className={`fas fa-comment-alt comment-${blog.id}`}  onClick={openCommentBox}></i>
+                        <i className={`fas fa-comment-alt comment-${blog.fbId}`}  onClick={openCommentBox}></i>
                         {blogComments?<span>{blogComments.length}</span>:null}
                     </div>
-                    {/* <div className={`btn-like-${blog.id}`} onClick={addLike}>
+                    <div>
+                        <div className="likes_by_opener" id={`likes_by_opener-${blog.fbId}`} onClick = {openLikesDisplay}>
+                        <i class="fas fa-box-open"></i>
+                        </div>
+                    <div className={`btn-like-${blog.fbId}`} onClick={addLike}>
                         {
                             !didLike?
                             <i className="fas fa-thumbs-up"></i>:
                             <i className="fas fa-thumbs-up liked"></i>
                         }
                         <span>{likes.size}</span>
-                    </div> */}
-                </div>
-                    {/* <div className={`blog-reactions-close-${blog.id}`} onClick={closeCommentBox}>
-                       <b>
-                            X
-                        </b>
-                    </div> */}
-                        <div className='comments-box'>
-                            { blogComments?
-                                blogComments.map(cmt=>(
-                                    <div className='comment-content'>
-                                        <p className='comment-text'>{cmt.comment}</p>
-                                        <div>
-                                       { cmt.by?
-                                        <Link
-                                        to={
-                                            {
-                                                pathname:`/profile/user-${cmt.by.id}`,
+                    </div>
+                    </div>
+                    <div className="likes_by_displayer" id={`likes_by_displayer-${blog.fbId}`}>
+                            <p onClick = {closeLikesDisplay} className='close-btn' id={`close-likesbtn-${blog.fbId}`}>
+                            <i class="far fa-window-close"></i>
+                            </p>
+                           { 
+                           [...likes].map(userId=>(
+                               <p>
+
+                                <Link
+                                    to={
+                                        {
+                                            pathname:`/profile/user-${userId}`,
                                             state:{
                                                 fromNavbar:false
                                             }
                                         }
-                                        }>
-                                        <br/>
-                                        <span className='comment-author'>by- {cmt.by.displayName}
-                                        </span>
-                                        </Link>:null
+                                    }><i class="ri-map-pin-user-fill"></i></Link>
+                                </p>
+                                ))
+                                }
+                        
+                    </div>
+                </div>
+                <div className="blog-comment-input" id= {`blog-comment-input-${blog.fbId}`}>
+                    <div className='blog-reactions-close' id={`blog-reactions-close-${blog.fbId}`} onClick={closeCommentBox}>
+                       <p className='close-btn'>
+                       <i class="far fa-window-close"></i>
+                        </p>
+                    </div>
+                    <div className='comments-box' id={`comments-box-${blog.fbId}`}>
+                        { blogComments?
+                            blogComments.map(cmt=>(
+                                <div className='comment-content'>
+                                    <p className='comment-text'>{cmt.comment}</p>
+                                    <div>
+                                    { cmt.by?
+                                    <Link
+                                    to={
+                                        {
+                                            pathname:`/profile/user-${cmt.by.id}`,
+                                        state:{
+                                            fromNavbar:false
+                                        }
                                     }
-                                    </div>
-    
-                                    </div>
-                                )):null
-                            }
-                        </div>
-                <div className={`blog-comment-input blog-comment-input-${blog.id}`}>
+                                    }>
+                                    <br/>
+                                    <span className='comment-author'>by- {cmt.by.displayName}
+                                    </span>
+                                    </Link>:null
+                                }
+                                </div>
+                                </div>
+                            )):null
+                        }
                     <form onSubmit ={postComment} className='blog-comment-form'>
                         <textarea minLength='5' maxLength='100'
-                        rows='3'
-                        placeholder="give your feed back about the blog"
-                        value={comment}
-                        onChange={handleCommentChange}
+                            rows='3'
+                            placeholder="give your feed back about the blog"
+                            value={comment}
+                            onChange={handleCommentChange}
                         >
                         </textarea>
                         <button type='submit'><i class="fas fa-share"></i></button>
                     </form>
+                    </div>
                 </div>
     </div>
     )

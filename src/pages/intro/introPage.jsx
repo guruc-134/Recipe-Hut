f4f775da8fe250a5fb1a138cfe445231f98d9411
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-07-05 00:58:32 +0530

    added intro page for the website

--- src/pages/intro/introPage.jsx
+++ src/pages/intro/introPage.jsx
@@ -4,142 +4,100 @@ import ReactTooltip from "react-tooltip";
 
 function Intro() {
     return (
-        <div class='intro'>
+        <div className='intro'>
+            <div className='star-repo'>
+                If you liked the experience, consider staring the repository
+                <a href='https://github.com/guruc-134/Recipe-Hut'>
+                <i className="fas fa-star"></i>
+                </a>
+            </div>
+            <div className='pageLogo'>
+                <img src={`${process.env.PUBLIC_URL}/logo_RH.png`} alt='recipeHut' />
+            </div>
+            <br></br>
                 <h2>
-                    Take a tour about this web application
+                    Some information that you might be looking for
                 </h2>
-              <div class="slider-intro">
-                {/*  put tool tip */}
+              <div className="slider-intro">
                 <div className='tags'>        
-                <i class=""></i>
                 <a  href="#1" ><i data-tip data-for="1" className="ri-number-1"></i></a>
                 <a href="#2"><i  data-tip data-for="2" className="ri-number-2"></i></a>
-                <a  href="#3" ><i data-tip data-for="3" className="ri-number-3"></i></a>
-                <a href="#4"><i  data-tip data-for="4" className="ri-number-4"></i></a>
-                <a  href="#5" ><i data-tip data-for="5" className="ri-number-5"></i></a>
                 <a href="#developer"><i  data-tip data-for="developer" className="fas fa-laptop-code"></i></a>
                 {/* <a href="#ingredients"><i className="fas fa-utensils"></i></a> */}
                 </div>
 
-                <div class="slides-intro">
-                    <div id="1">    
-                        Login using your gmail or create an account for this website
-                        <img></img>
-                        
-                    </div>
-                    <div id="2">
-                         lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    </div>
-                    <div id="3">
-                         lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
+                <div className="slides-intro">
+                    <div id ='1'>
+                        <h2>User Perspective</h2>
+                        <div className='keywords user_perspective_keywords'>
+                            <span>Sign in to Recipe Hut</span>
+                            <span> Create account</span>
+                            <span>Set up your profile</span>
+                            <span>Get Random Recipes Feed</span>
+                            <span>Customize the colors of your site</span>
+                            <span>Search for recipes and get the answers as blogs and videos</span>
+                            <span>View others profile</span>
+                            <span>Write blogs</span>
+                            <span>Join a community of cooking enthusiasts and share your recipes</span>
+                            <span>get  random facts</span>
+                            <span>Talk to chef and get nutritional information</span>
+                            <span>Share recipes and profile of others</span>
+                            <span> Show your appreciation through liking and commenting on others blogs</span>
                         </div>
-                    <div id="4">    
-                     lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
                     </div>
-                    <div id="5">
-                         lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
+                    <div id = '2' >
+                        <h2>Facing Problems ?
+                        </h2>
+                        <h4>If you are facing any of the problems such as</h4>
+                        <div className='keywords'>
+                        <span>- not able to set profile picture </span>
+                        <span>- not able to fetch blogs of the day</span>
+                        <span>- not able to search for your favourite recipe</span>
+                        <span>- or similar issues</span>
+                        <p> It could either be due to overloading or the API call's, or exhaustion of firebase's daily quota for number of requests, please visit the site after some time</p>
+                            </div>
                     </div>
-                    <div id="developer">
-                         lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
+                    <div id='developer'>
+                            <h2> Hello,👋 curious developer</h2>
+                            <h4> These are some keywords that you might find interesting </h4>
+                            <div className='developer-keywords keywords'>
+                                    <span>React</span>
+                                    <span>Sass</span>
+                                    <span>Firebase</span>
+                                    <span>Firestore</span>
+                                    <span>Glassmorphism</span>
+                                    <span>User Authentication</span>
+                                    <span>Speech Recognition</span>
+                                    <span>Session Storage</span>
+                                    <span>React Toastify</span>
+                                    <span>React Hooks</span>
+                                    <span>React Router DOM</span>
+                                    <span>Pagination</span>
+                                    <span>REST API's</span>
+                                    <span>Axios</span>
+                                    <span>React Dom</span>
+                                    <span>React Tooltip</span>
+                                    <span>React Loading</span>
+                                    <span>Css Animations</span>
+                                    <span>SVG components</span>
+                                    <span>File transfer</span>
+                                    <span>Font Awesome</span>
+                                    <span>Remix icons</span>
+                            </div>
+                    </div>
+                    <div>
 
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
-
-                    lorem a aS;DNA;SLDN ADada d ADASKDL ADKB ASDLK AB ADSKLHA bsdlaksb afslk abl kahs la
                     </div>
                 </div>
                 </div>
                 <ReactTooltip id="1" place="bottom" effect="solid" >
                     part one
-                </ReactTooltip><ReactTooltip id="2" place="bottom" effect="solid" >
+                </ReactTooltip>
+                <ReactTooltip id="2" place="bottom" effect="solid" >
                     part two 
-                </ReactTooltip><ReactTooltip id="3" place="bottom" effect="solid" >
-                    part three   
-                </ReactTooltip><ReactTooltip id="4" place="bottom" effect="solid" >
-                    part  four  
-                </ReactTooltip><ReactTooltip id="5" place="bottom" effect="solid" >
-                    part  five  
-                </ReactTooltip><ReactTooltip id="developer" place="bottom" effect="solid" >
+                </ReactTooltip>
+                
+                <ReactTooltip id="developer" place="bottom" effect="solid" >
                     info for programmers
                 </ReactTooltip>
         </div>
